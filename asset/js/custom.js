1627fd263d4065d72aa640561884200d58d93f62
devshisir <contact.shisirimran@gmail.com>
2021-04-04 04:55:07 +0600

    Home page work done

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,5 +1,71 @@
 $(document).ready(function() {
-    var school = $(window);
+    var carpage = $(window);
     var page = $('html, body');
+
+
+    // package slider active 
+    $('.package_slider_active').owlCarousel({
+        loop:true,
+        margin:30,
+        responsiveClass:true,
+        items: 3,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive:{
+            0:{
+                
+            },
+            600:{
+               
+            },
+            1000:{
+                
+            }
+        }
+    })
+
+    // testmonial slider area 
+    $('.testmonial_slider_active').owlCarousel({
+        loop:true,
+        margin:0,
+        responsiveClass:true,
+        items: 1,
+        autoplay: true,
+        smartSpeed: 1000,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        responsive:{
+            0:{
+                
+            },
+            600:{
+               
+            },
+            1000:{
+                
+            }
+        }
+    })
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 });
